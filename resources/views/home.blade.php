4cfaef3fe4475558279abb30eb26188de168bd91
ironkentank <akunbuatunduh69@gmail.com>
2023-11-15 22:51:24 +0700

    dashboard > optimize2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,48 +5,27 @@
 <div data-bs-spy="scroll" class="scrollspy-example">
     <!-- Hero: Start -->
     <section id="hero-animation">
-      <div id="landingHero" class="section-py landing-hero position-relative">
+      <div id="landingCTA" class="section-py landing-hero position-relative">
         <div class="container">
-          <div class="hero-text-box text-center">
-            <h1 class="text-primary hero-title display-6 fw-bold">One dashboard to manage all your businesses</h1>
-            <h2 class="hero-sub-title h6 mb-4 pb-1">
-              Production-ready & easy to use Admin Template<br class="d-none d-lg-block" />
-              for Reliability and Customizability.
-            </h2>
-            <div class="landing-hero-btn d-inline-block position-relative">
-              <span class="hero-btn-item position-absolute d-none d-md-flex text-heading"
-                >Join community
+            <div class="row align-items-center gy-5 gy-lg-0">
+              <div class="col-lg-6 text-center text-lg-start">
+                <h6 class="h2 text-primary fw-bold mb-1">Ready to Get Started?</h6>
+                <p class="fw-medium mb-4">Start your project with a 14-day free trial</p>
+                <a href="payment-page.html" class="btn btn-lg btn-primary">Get Started</a>
+              </div>
+              <div class="col-lg-6 pt-lg-5 text-center text-lg-end">
                 <img
-                  src="/assets/img/front-pages/icons/Join-community-arrow.png"
-                  alt="Join community arrow"
-                  class="scaleX-n1-rtl"
-              /></span>
-              <a href="#landingPricing" class="btn btn-primary btn-lg">Get early access</a>
+                  src="/assets/img/front-pages/landing-page/cta-dashboard.png"
+                  alt="cta dashboard"
+                  class="img-fluid" />
+              </div>
             </div>
           </div>
-          <div id="heroDashboardAnimation" class="hero-animation-img">
-            <a href="/asset/vertical-menu-template/app-ecommerce-dashboard.html" target="_blank">
-              <div id="heroAnimationImg" class="position-relative hero-dashboard-img">
-                <img
-                  src="/assets/img/front-pages/landing-page/hero-dashboard-light.png"
-                  alt="hero dashboard"
-                  class="animation-img"
-                  data-app-light-img="front-pages/landing-page/hero-dashboard-light.png"
-                  data-app-dark-img="front-pages/landing-page/hero-dashboard-dark.png" />
-                <img
-                  src="/assets/img/front-pages/landing-page/hero-elements-light.png"
-                  alt="hero elements"
-                  class="position-absolute hero-elements-img animation-img top-0 start-0"
-                  data-app-light-img="front-pages/landing-page/hero-elements-light.png"
-                  data-app-dark-img="front-pages/landing-page/hero-elements-dark.png" />
-              </div>
-            </a>
-          </div>
-        </div>
       </div>
-      <div class="landing-hero-blank"></div>
     </section>
     <!-- Hero: End -->
+    <!-- CTA: Start -->
+    <!-- CTA: End -->
 
     <!-- Useful features: Start -->
     {{-- <section id="landingFeatures" class="section-py landing-features">
@@ -864,25 +843,7 @@
     </section>
     <!-- FAQ: End -->
 
-    <!-- CTA: Start -->
-    <section id="landingCTA" class="section-py landing-cta p-lg-0 pb-0">
-      <div class="container">
-        <div class="row align-items-center gy-5 gy-lg-0">
-          <div class="col-lg-6 text-center text-lg-start">
-            <h6 class="h2 text-primary fw-bold mb-1">Ready to Get Started?</h6>
-            <p class="fw-medium mb-4">Start your project with a 14-day free trial</p>
-            <a href="payment-page.html" class="btn btn-lg btn-primary">Get Started</a>
-          </div>
-          <div class="col-lg-6 pt-lg-5 text-center text-lg-end">
-            <img
-              src="/assets/img/front-pages/landing-page/cta-dashboard.png"
-              alt="cta dashboard"
-              class="img-fluid" />
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- CTA: End -->
+
 
     <!-- Contact Us: Start -->
     <section id="landingContact" class="section-py bg-body landing-contact">
