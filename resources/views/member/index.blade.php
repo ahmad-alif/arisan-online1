aa708e544e9724175ab4df80a8ea2b582d475ea0
ironkentank <akunbuatunduh69@gmail.com>
2023-11-23 23:51:20 +0700

    fix padding title manage member owner

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -7,7 +7,7 @@
         <!-- Content -->
 
         <div class="container-xxl flex-grow-1 container-p-y">
-            <h4 class="py-3 mb-4"><span class="text-muted fw-light">Admin /</span> Kelola Anggota</h4>
+            <h4 class="mb-4"><span class="text-muted fw-light">Admin /</span> Kelola Anggota</h4>
 
             <div class="card">
                 <div class="table-responsive text-nowrap">
