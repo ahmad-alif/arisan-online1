20f40b935135f550ab4c143a9ebcc8d32261890d
ironkentank <akunbuatunduh69@gmail.com>
2023-11-26 19:59:49 +0700

    fix footer dashboard

--- resources/views/layouts/footer_dashboard.blade.php
+++ resources/views/layouts/footer_dashboard.blade.php
@@ -2,13 +2,13 @@
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl">
       <div
-        class="footer-container d-flex align-items-center justify-content-between py-2 flex-md-row flex-column">
+        class="footer-container d-flex align-items-right justify-content-between py-2 flex-md-row flex-column">
         <div>
           ©
           <script>
             document.write(new Date().getFullYear());
           </script>
-          by <a href="https://mediasaranadigitalindo.com/" target="_blank" class="fw-medium">Media Sarana Digitalindo</a>
+          by <a href="https://mediasaranadigitalindo.com/" target="_blank" class="fw-medium">MSD</a>
         </div>
       </div>
     </div>
