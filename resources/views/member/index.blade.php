333a07eb437b2bf8130de0acab6b26d562c82501
ironkentank <akunbuatunduh69@gmail.com>
2023-11-27 01:35:21 +0700

    fix detail manage member owner

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -60,45 +60,75 @@
                 @foreach ($members as $member)
                     <div class="modal fade" id="memberInfoModal-{{ $member->id }}" tabindex="-1" role="dialog"
                         aria-labelledby="memberInfoModalLabel-{{ $member->id }}" aria-hidden="true">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="memberInfoModalLabel-{{ $member->id }}">
-                                        Informasi Member dan Arisan yang Diikuti</h5>
-                                </div>
+                        <div class="modal-dialog modal-dialog-centered modal-simple modal-add-new-cc">
+                            <div class="modal-content p-3 p-md-5">
                                 <div class="modal-body">
-                                    <div class="row">
-                                        <div class="col-md-4">
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                    <div class="text-center mb-4">
+                                        <h2 class="mb-2">{{ $member->username }}</h2>
+                                    </div>
+                                    <div class="row g-3">
+                                        <div class="col-12 text-center">
                                             @if ($member->foto_profil)
-                                                <img src="{{ Storage::url($member->foto_profil) }}" alt="Profile"
-                                                    class="rounded-circle" width="150">
+                                                <img src="{{ Storage::url($member->foto_profil) }}"
+                                                    alt="Foto Profil" class="rounded-circle" width="100">
                                             @else
-                                                <img src="{{ asset('img/default.png') }}" alt="Default Profile"
-                                                    class="rounded-circle" width="100">
+                                                <img src="{{ asset('img/default.png') }}"
+                                                    alt="Default Profile" class="rounded-circle"
+                                                    width="100">
                                             @endif
                                         </div>
-                                        <div class="col-md-8">
-                                            <p><strong>Username:</strong> {{ $member->username }}</p>
-                                            <p><strong>Name:</strong> {{ $member->name }}</p>
-                                            <p><strong>Email:</strong> {{ $member->email }}</p>
-                                            <p><strong>No HP:</strong> {{ $member->nohp }}</p>
-                                            <p><strong>Foto KTP:</strong></p>
-                                            @if ($member->foto_ktp == null)
-                                                <p>Foto KTP Belum Ada</p>
+                                        <div class="col-12">
+                                            <label class="form-label">Nama: </label>
+                                            <p>{{ $member->name }}</p>
+                                        </div>
+                                        <div class="col-12">
+                                            <label class="form-label">Username: </label>
+                                            <p>{{ $member->username }}</p>
+                                        </div>
+                                        <div class="col-12">
+                                            <label class="form-label">Email: </label>
+                                            <p>{{ $member->email }}</p>
+                                        </div>
+                                        <div class="col-12">
+                                            <label class="form-label">No HP: </label>
+                                            <p>{{ $member->nohp }}</p>
+                                        </div>
+                                        <div class="row">
+                                            <label class="form-label">Arisan yg diikuti: </label>
+                                            @if ($member->joinedArisans->isEmpty())
+                                                <p>{{ $member->name }} belum mengikuti arisan.</p>
                                             @else
-                                                <img src="{{ Storage::url($member->foto_ktp) }}" alt="KTP"
-                                                    width="250">
+                                                @foreach ($member->joinedArisans as $arisan)
+                                                    {{-- <div class="col-md-4 mb-3"> --}}
+                                                    <div class="col-xl-4 col-md-4 col-6 mb-3">
+                                                        <div class="card">
+                                                            {{-- <img src="{{ Storage::url($arisan->img_arisan) }}"
+                                                            class="card-img-top" alt="Arisan Image"> --}}
+                                                            <div
+                                                                style="position: relative; width: 100%; padding-bottom: 100%;">
+                                                                <img style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;"
+                                                                    class="border rounded img-fluid"
+                                                                    src="{{ Storage::url($arisan->img_arisan) }}"
+                                                                    alt="{{ $arisan->nama_arisan }}" />
+                                                            </div>
+                                                            <div class="card-body">
+                                                                <h5 class="card-title"
+                                                                    style="font-size: 14px; margin-left: -8px;">
+                                                                    {{ Str::limit($arisan->nama_arisan, 10, '...') }}
+                                                                </h5>
+                                                                <p class="card-text"
+                                                                    style="font-size: 12px; margin-left: -8px;">
+                                                                    {{ Str::limit($arisan->deskripsi, 12, '...') }}
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                @endforeach
                                             @endif
-                                            <p><strong>Arisan yang Diikuti:</strong></p>
-                                            @foreach ($member->arisans as $arisan)
-                                                <p>{{ $arisan->nama_arisan }}</p>
-                                                <!-- ... (tampilkan informasi arisan lainnya sesuai kebutuhan Anda) ... -->
-                                            @endforeach
                                         </div>
                                     </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                                 </div>
                             </div>
                         </div>
