51ac3359e0a6f0a8aa0b88620f7046ae2786bc3c
Ahmad Alif <ahmadalif8990@gmail.com>
2023-12-05 20:55:11 +0700

    update view choose role

--- resources/views/auth/choose-role.blade.php
+++ resources/views/auth/choose-role.blade.php
@@ -1,175 +1,124 @@
 <!DOCTYPE html>
 
-<html
-  lang="en"
-  class="light-style layout-navbar-fixed layout-wide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="/assets/"
-  data-template="front-pages">
-  <head>
+<html lang="en" class="light-style layout-navbar-fixed layout-wide" dir="ltr" data-theme="theme-default"
+    data-assets-path="/assets/" data-template="front-pages">
+
+<head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Pilih Role anda | Arisanku</title>
 
     <meta name="description" content="" />
-
-    <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
-
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
         rel="stylesheet" />
-
-    <!-- Icons -->
     <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
     <link rel="stylesheet" href="../../assets/vendor/fonts/tabler-icons.css" />
     <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
-
-    <!-- Core CSS -->
     <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
     <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="../../assets/css/demo.css" />
-
-    <!-- Vendors CSS -->
     <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
     <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
     <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
-    <!-- Vendor -->
     <link rel="stylesheet" href="../../assets/vendor/libs/@form-validation/umd/styles/index.min.css" />
-
-    <!-- Page CSS -->
-    <!-- Page -->
     <link rel="stylesheet" href="../../assets/vendor/css/pages/page-auth.css" />
-
-    <!-- Helpers -->
     <script src="../../assets/vendor/js/helpers.js"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
     <script src="../../assets/vendor/js/template-customizer.js"></script>
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="../../assets/js/config.js"></script>
-  </head>
+</head>
 
-  <body>
+<body>
     <script src="/assets/vendor/js/dropdown-hover.js"></script>
     <script src="/assets/vendor/js/mega-dropdown.js"></script>
 
-    <!-- Navbar: Start -->
-
-    <!-- Navbar: End -->
-
-    <!-- Sections:Start -->
-
     <div data-bs-spy="scroll" class="scrollspy-example">
-      <!-- Hero: Start -->
-
-      <!-- Hero: End -->
-
-      <!-- Useful features: Start -->
-
-      <!-- Useful features: End -->
-
-      <!-- Real customers reviews: Start -->
-
-      <!-- Real customers reviews: End -->
-
-      <!-- Our great team: Start -->
-      <!-- Our great team: End -->
-
-      <!-- Pricing plans: Start -->
-
         <div class="container">
-          <div class="text-center mb-3 pb-1 mt-5">
-            <span class="badge bg-label-primary">Pilih Peran</span>
-          </div>
-          <h3 class="text-center mb-1"><span class="section-title">Pilih peran anda</span> dalam arisan</h3>
-          <p class="text-center mb-4 pb-3">
-            Sebelum mendaftar tentukan dahulu apakah anda mau menjadi pemilik arisan atau pengguna arisan.
-          </p>
-          <div class="row gy-4 pt-lg-0">
-          <form class="needs-validation d-flex justify-content-center" method="POST"
-            action="{{ route('register.selectRole') }}">
-            @csrf
-            <!-- Basic Plan: Start -->
-            <div class="col-3">
-              <div class="card m-1 h-100 ">
-                <div class="card-header p-3">
-                  <div class="text-center">
-
-                        <img
-                          class="img-fluid mt-2"
-                          src="../../assets/img/illustrations/boy-app-academy.png"
-                          alt="Card girl image"
-                          width="290"
-                           />
-
-                  </div>
-                </div>
-                <div class="card-body">
-                  <div class="d-grid">
-                    <button type="submit" name="role" value="user" class="btn btn-primary">Pengguna Arisan</button>
-                  </div>
-                </div>
-              </div>
+            <div class="text-center mb-3 pb-1 mt-5">
+                <span class="badge bg-label-primary">Pilih Peran</span>
             </div>
-            <!-- Basic Plan: End -->
+            <h3 class="text-center mb-1"><span class="section-title">Pilih peran anda</span> dalam arisan</h3>
+            <p class="text-center mb-4 pb-3">
+                Sebelum mendaftar tentukan dahulu apakah anda mau menjadi pemilik arisan atau pengguna arisan.
+            </p>
+            <div class="row gy-4 pt-lg-0">
+                <form class="needs-validation d-flex justify-content-center" method="POST"
+                    action="{{ route('register.selectRole') }}">
+                    @csrf
+                    <!-- Basic Plan: Start -->
+                    <div class="col-lg-4 col-md-12">
+                        <div class="card m-1">
+                            <div class="card-header p-3">
+                                <div class="text-center">
 
-            <!-- Favourite Plan: Start -->
-            <div class="col-3">
-              <div class="card m-1 h-100 ">
-                <div class="card-header p-3">
-                  <div class="text-center">
+                                    <img class="img-fluid mt-2" src="../../assets/img/illustrations/boy-app-academy.png"
+                                        alt="Card girl image" width="290" />
 
-                        <img
-                          class="img-fluid mt-2"
-                          src="../../assets/img/illustrations/girl-app-academy.png"
-                          alt="Card girl image"
-                          width="290"
-                           />
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="d-grid">
+                                    <button type="submit" name="role" value="user"
+                                        class="btn btn-primary">Pengguna Arisan</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Basic Plan: End -->
 
-                  </div>
-                </div>
-                <div class="card-body">
-                  <div class="d-grid">
-                  <button type="submit" name="role" value="owner" class="btn btn-danger">Pemilik Arisan</button>
-                  </div>
-                </div>
-              </div>
+                    <!-- Favourite Plan: Start -->
+                    <div class="col-lg-4 col-md-12">
+                        <div class="card m-1">
+                            <div class="card-header p-3">
+                                <div class="text-center">
+
+                                    <img class="img-fluid mt-2"
+                                        src="../../assets/img/illustrations/girl-app-academy.png" alt="Card girl image"
+                                        width="290" />
+
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="d-grid">
+                                    <button type="submit" name="role" value="owner" class="btn btn-danger">Pemilik
+                                        Arisan</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Favourite Plan: End -->
+
+                    <!-- Standard Plan: Start -->
+                    <!-- Standard Plan: End -->
+                </form>
+                <h4 class="text-center mb-0 mt-4">Sudah punya akun?</h4>
+                <div class="d-flex justify-content-center mt-2"> <a href="{{ route('login') }}"
+                        class="btn btn-primary mb-5">Masuk di sini!</a></div>
             </div>
-            <!-- Favourite Plan: End -->
-
-            <!-- Standard Plan: Start -->
-            <!-- Standard Plan: End -->
-            </form>
-            <h4 class="text-center mb-0 mt-4">Sudah punya akun?</h4>
-            <div class="d-flex justify-content-center mt-2"> <a href="{{ route('login') }}" class="btn btn-primary mb-5">Masuk di sini!</a></div>
-          </div>
         </div>
 
-      <!-- Pricing plans: End -->
+        <!-- Pricing plans: End -->
 
-      <!-- Fun facts: Start -->
+        <!-- Fun facts: Start -->
 
-      <!-- Fun facts: End -->
+        <!-- Fun facts: End -->
 
-      <!-- FAQ: Start -->
+        <!-- FAQ: Start -->
 
-      <!-- FAQ: End -->
+        <!-- FAQ: End -->
 
-      <!-- CTA: Start -->
+        <!-- CTA: Start -->
 
-      <!-- CTA: End -->
+        <!-- CTA: End -->
 
-      <!-- Contact Us: Start -->
+        <!-- Contact Us: Start -->
 
-      <!-- Contact Us: End -->
+        <!-- Contact Us: End -->
     </div>
 
     <!-- / Sections:End -->
@@ -195,5 +144,6 @@
 
     <!-- Page JS -->
     <script src="/assets/js/front-page-landing.js"></script>
-  </body>
+</body>
+
 </html>
